b0f2775b653bff4e30c920065eab6424ea822ac2
joaolucenalima <joaovitorlucena000@gmail.com>
2024-12-20 00:51:09 -0300

    feat: static data added in preview table

--- src/components/table/preview-table.tsx
+++ src/components/table/preview-table.tsx
@@ -1,9 +1,26 @@
-import { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
 import { ModeContext } from "../../contexts/mode-context"
-import "./table.css"
+import "./preview-table.css"
+
+type PostsType = {
+  userId: number
+  id: number
+  title: string
+  body: string
+}
 
 export function PreviewTable() {
   const { tableColumns, tableConfiguration } = useContext(ModeContext)
+
+  const [posts, setPosts] = useState<PostsType[]>()
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then(response => response.json())
+      .then((data: PostsType[]) => {
+        setPosts(data.slice(0, 5))
+      })
+  }, [])
 
   return (
     <div className="table_container">
@@ -12,18 +29,30 @@ export function PreviewTable() {
       </div>
 
       <div className="table_content">
-        {tableColumns.map(column => (
-          <div className="preview_table_column" style={{ textAlign: column.textAlign }}>
-            <div className="column_header">
-              <span className="column_title">
-                {column.title}
-              </span>
-            </div>
+        <table>
+          <thead>
+            <tr>
+              {tableColumns.map((column, index) => (
+                <th key={index} style={{ textAlign: column.textAlign }}>
+                  {column.title}
+                </th>
+              ))}
+            </tr>
+          </thead>
 
-            <div className="column_content"></div>
-          </div>
-        ))}
+          <tbody>
+            {Array.from({ length: 5 }).map((_, rowIndex) => (
+              <tr key={rowIndex}>
+                {tableColumns.map((column, colIndex) => (
+                  <td key={colIndex} style={{ textAlign: column.textAlign }}>
+                    {posts ? posts[rowIndex]?.title || "-" : "-"}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
-    </div>
+    </div >
   )
 }
